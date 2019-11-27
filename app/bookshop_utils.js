d9c05511d63ff8b0a58d67fbb81040a987b75b06
Simone Klein <simoneandpaper@gmail.com>
2019-11-27 18:13:08 +0000

    Programming Fundamentals 001

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,35 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(firstname, surname) {
+    return firstname + ' ' + surname;
+}
+
+function makeHalfPrice(price) {
+    return (price/2);
+}
+
+function countBooks(array) {
+    return (array.length);
+}
+
+function isInStock(book) {
+    return (book.quantity > 0);
+}
+
+/* Put in all parameters being counted (a, b, c),
+as input into test (quantity, price, VAT)*/
+function getTotalOrderPrice(quantity, price, VAT) {
+    let exVAT = quantity*price;
+// ? How do we know what 'VAT' is?  
+    return exVAT + (VAT*exVAT);
+}
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
 };
